7080463706fec3506b20864e4a00a7eda11378c0
Espen A. Kristiansen <post@espenak.net>
2013-10-20 22:28:55 +0200

    apps.core.serialize.feedback: Do not cache.
    Makes no sense.

--- src/devilry/devilry/apps/core/serialize/feedback.py
+++ src/devilry/devilry/apps/core/serialize/feedback.py
@@ -1,10 +1,7 @@
 from devilry.utils.restformat import format_datetime
-from .cache import serializedcache
-from ..models import StaticFeedback
 
 
-
-def _serialize_feedback(feedback):
+def serialize_feedback(feedback):
     return {'id': feedback.id,
             'rendered_view': feedback.rendered_view,
             'save_timestamp': format_datetime(feedback.save_timestamp),
@@ -12,15 +9,8 @@ def _serialize_feedback(feedback):
             'is_passing_grade': feedback.is_passing_grade,
             'points': feedback.points}
 
-serializedcache.add(_serialize_feedback, {
-    StaticFeedback: None
-})
-
-
-def serialize_feedback(feedback):
-    return serializedcache.cache(_serialize_feedback, feedback)
 
 def serialize_feedback_without_points(feedback):
-    serialized = serializedcache.cache(_serialize_feedback, feedback)
+    serialized = serialize_feedback(feedback)
     del serialized['points']
     return serialized
