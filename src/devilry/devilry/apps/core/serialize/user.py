56f582654856a591b34a73e0a4c3eb3847a55079
Espen A. Kristiansen <post@espenak.net>
2013-10-20 18:15:46 +0200

    apps.core.serialize.user: Use cache.

--- src/devilry/devilry/apps/core/serialize/user.py
+++ src/devilry/devilry/apps/core/serialize/user.py
@@ -1,6 +1,22 @@
-def serialize_user(user):
+from django.contrib.auth.models import User
+
+from ..models.devilryuserprofile import DevilryUserProfile
+from .cache import serializedcache
+
+
+
+def _serialize_user(user):
     return {'email': user.email,
             'username': user.username,
             'id': user.id,
             'full_name': user.get_profile().full_name,
             'displayname': user.get_profile().full_name or user.username}
+
+serializedcache.add(_serialize_user, {
+    User: None,
+    DevilryUserProfile: lambda p: p.user
+})
+
+
+def serialize_user(user):
+    return serializedcache.cache(_serialize_user, user)
