47eac9df30833cb158a3eb2144b28226ca180ad6
Espen A. Kristiansen <post@espenak.net>
2013-10-21 14:03:56 +0200

    devilry_bootstrapbase: Working extjs app example.

---
+++ src/devilry_bootstrapbase/devilry_bootstrapbase/static/devilry_bootstrapbase/app.js
@@ -0,0 +1,42 @@
+Ext.application({
+    name: 'devilry_bootstrapbase',
+    appFolder: DevilrySettings.DEVILRY_STATIC_URL + '/devilry_bootstrapbase/app',
+    paths: {
+        'devilry_extjsextras': DevilrySettings.DEVILRY_STATIC_URL + '/devilry_extjsextras'
+    },
+
+    requires: [
+        'devilry_extjsextras.Router',
+        'devilry_extjsextras.RouteNotFound'
+    ],
+
+    controllers: [],
+
+    launch: function() {
+        this._setupRoutes();
+    },
+
+    /** Used by controllers to set the page title (the title-tag). */
+    setTitle: function(title) {
+        window.document.title = Ext.String.format('{0} - Devilry', title);
+    },
+
+
+    /*********************************************
+     * Routing
+     ********************************************/
+
+    _setupRoutes: function() {
+        this.route = Ext.create('devilry_extjsextras.Router', this);
+        this.route.add("", 'dashboard');
+        this.route.start();
+    },
+    
+    routeNotFound: function(routeInfo) {
+        console.error('Route not found:', routeInfo.token);
+    },
+
+    dashboard: function() {
+        console.log('dashboard route triggered');
+    }
+});
--- src/devilry_settings/devilry_settings/default_settings.py
+++ src/devilry_settings/devilry_settings/default_settings.py
@@ -98,6 +98,7 @@ TEMPLATE_CONTEXT_PROCESSORS = ("django.contrib.auth.context_processors.auth",
                                "django.core.context_processors.media",
                                "django.core.context_processors.debug",
                                'django.contrib.messages.context_processors.messages',
+                               'django.core.context_processors.static',
                                'extjs4.context_processors.extjs4',
                                'devilry.apps.theme.templatecontext.template_variables')
 
