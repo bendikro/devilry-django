854244bacd94ce8035537c6d6fa289747b511956
Tor Ivar Johansen <toij@ifi.uio.no>
2014-05-21 20:53:56 +0200

    devilry_student: Breadcrumbs

--- src/devilry_student/devilry_student/views/deliverydetails.py
+++ src/devilry_student/devilry_student/views/deliverydetails.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from django.views.generic import DetailView
+from devilry.apps.core.models import Delivery
 
-class DeliveryDetailsView(TemplateView):
+class DeliveryDetailsView(DetailView):
+    model = Delivery
     template_name = "devilry_student/deliverydetails.django.html"
+    pk_url_kwargs = 'pk'
+
+    
